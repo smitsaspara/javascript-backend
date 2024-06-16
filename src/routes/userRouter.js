d585f4be4b6e9b527da84b52961ca900727072af
Smit Saspara <smit@Smits-MBP.lan>
2024-06-16 08:22:45 -0500

    created user routers

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { logOutUser, loginUser, registerUser,refreshAccessToken } from "../controllers/userController.js";
+import { logOutUser, loginUser, registerUser,refreshAccessToken, changePassword, getUser, updateUserDetails, updateAvatar, updateCoverImage, userProfile, getWatchHistory } from "../controllers/userController.js";
 import { upload } from "../middlewares/multerMiddleWare.js";
 import { varifyJWT } from "../middlewares/authMiddleWare.js";
 
@@ -21,5 +21,19 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(varifyJWT,logOutUser);
 
 router.route("/accessToken").post(refreshAccessToken);
+
+router.route("/changePassword").post(varifyJWT, changePassword)
+
+router.route("/currentUser").get(varifyJWT, getUser)
+
+router.route("/updateAccount").patch(varifyJWT, updateUserDetails)
+
+router.route("/avatar").patch(varifyJWT, upload.single("avatar"), updateAvatar)
+
+router.route("/coverImage").patch(varifyJWT, upload.single("coverImage"), updateCoverImage)
+
+router.route("/channel/:username").get(varifyJWT, userProfile )
+
+router.route("/history").get(varifyJWT, getWatchHistory)
 
 export default router;
